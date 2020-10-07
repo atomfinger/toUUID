9a2b95badd2cc3eb7d262d0c985ef1354e4304d6
John Mikael Gundersen <john@jmgundersen.com>
2020-10-07 12:53:02 +0200

    #2 Creating in-code documentation

--- src/main/java/com/atomfinger/touuid/UUIDs.java
+++ src/main/java/com/atomfinger/touuid/UUIDs.java
@@ -9,24 +9,65 @@ import java.util.stream.IntStream;
 
 public class UUIDs {
 
+    /**
+     * Converts an int to a UUID
+     * <p>
+     * 1 is converted to "00000000-0000-0000-0000-000000000001"
+     * 10 is converted to "00000000-0000-0000-0000-000000000010"
+     * 15 is converted to "00000000-0000-0000-0000-000000000015"
+     * and so forth.
+     * <p>
+     * A negative integer will result in a null UUID:
+     * -1 is converted to "00000000-0000-0000-0000-000000000000"
+     * <p>
+     * Example:
+     * UUIDs.fromInt(1);
+     *
+     * @param positiveNumber: The int to convert
+     * @return the generated UUID
+     */
     public static UUID fromInt(int positiveNumber) {
         return toUUID(positiveNumber);
     }
 
+    /**
+     * Generates a list of UUIDs based on int inputs
+     * <p>
+     * Example:
+     * UUIDs.fromInts(1, 2, 3);
+     *
+     * @param positiveNumbers: The numbers to convert
+     * @return List of generated UUIDs
+     */
     public static List<UUID> fromInts(int... positiveNumbers) {
         return Arrays.stream(positiveNumbers).mapToObj(UUIDs::toUUID).collect(Collectors.toList());
     }
 
+    /**
+     * Generates a list of UUIDs based on a collection of integers
+     * <p>
+     * Example:
+     * UUIDs.fromInts(Arrays.asList(1, 2, 3));
+     *
+     * @param integers: The numbers to convert
+     * @return List of generated UUIDs
+     */
     public static List<UUID> fromInts(Collection<Integer> integers) {
         return integers.stream().map(UUIDs::toUUID).collect(Collectors.toList());
     }
 
+    /**
+     * Generates a list of UUIDs based on an inclusive range of ints
+     * <p>
+     * Example:
+     * UUIDs.fromRange(1, 3); //Will generate 3 UUIDs
+     *
+     * @param startInclusive: Start of range
+     * @param endInclusive:   End of range
+     * @return List of generated UUIDs
+     */
     public static List<UUID> fromRange(int startInclusive, int endInclusive) {
         return IntStream.rangeClosed(startInclusive, endInclusive).mapToObj(UUIDs::toUUID).collect(Collectors.toList());
-    }
-
-    private static List<UUID> toUUIDs(Collection<Integer> integers) {
-        return integers.stream().map(UUIDs::toUUID).collect(Collectors.toList());
     }
 
     private static UUID toUUID(Integer integer) {
--- src/main/kotlin/com/atomfinger/touuid/ToUUIDExtensions.kt
+++ src/main/kotlin/com/atomfinger/touuid/ToUUIDExtensions.kt
@@ -8,14 +8,61 @@ private const val paddingCharacter = '0'
 private fun generateUUIDString(i: Int) =
     "00000000-0000-0000-0000-${i.toString().padStart(totalLength, paddingCharacter)}"
 
+/**
+ * Converts the extended integer to a UUID
+ *
+ * 1 is converted to "00000000-0000-0000-0000-000000000001"
+ * 10 is converted to "00000000-0000-0000-0000-000000000010"
+ * 15 is converted to "00000000-0000-0000-0000-000000000015"
+ * and so forth.
+ *
+ * A negative integer will result in a null UUID:
+ * -1 is converted to "00000000-0000-0000-0000-000000000000"
+ *
+ *  EXAMPLE:
+ *   1.toUUID()
+ *
+ * @return Converted UUID
+ */
 fun Int.toUUID(): UUID = UUID.fromString(generateUUIDString(maxOf(this, 0)))
 
-fun Collection<Int>.toUUIDs() = this.map { it.toUUID() }
+/**
+ * Converts a collection of integers to a list of UUID
+ *
+ *  EXAMPLE:
+ *   listOf(1, 2, 3).toUUID()
+ *
+ *  @return List of UUIDs
+ */
+fun Collection<Int>.toUUIDs(): List<UUID> = this.map { it.toUUID() }
 
+/**
+ * Yields as many UUIDs as needed.
+ *
+ * EXAMPLE:
+ *  uuids().take(5)
+ *
+ * WARNING:
+ * While this function technically allows you to generate a couple of billions of UUIDs,
+ * that is not a supported use case. The indented use case is to create the UUIDs needed to satisfy a test case.
+ *
+ */
 fun uuids() = sequence {
     for (i in (1..Int.MAX_VALUE))
         yield(i.toUUID())
 }
 
+/**
+ * Converts a range of integers to UUIDs
+ *
+ * EXAMPLE:
+ *  (1..5).toUUID()
+ *
+ * WARNING:
+ * While this function technically allows you to generate a couple of billions of UUIDs,
+ * that is not a supported use case. The indented use case is to create the UUIDs needed to satisfy a test case.
+ *
+ *  @return List of UUIDs
+ */
 fun IntRange.toUUIDs(): List<UUID> = this.toList().toUUIDs()
 
